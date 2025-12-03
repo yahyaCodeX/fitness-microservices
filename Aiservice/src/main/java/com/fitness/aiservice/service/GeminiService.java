8f619090d3e362734386eb78836f2ef68d345f88
yahya <siddiquiyahya796@gmail.com>
2025-12-04 02:49:51 +0500

    Gemini Service added to generate reponses by using GeminiApiKey

---
+++ Aiservice/src/main/java/com/fitness/aiservice/service/GeminiService.java
@@ -0,0 +1,42 @@
+package com.fitness.aiservice.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class GeminiService {
+
+    private final WebClient webClient;
+
+    @Value("${gemini.api.url}")
+    private String geminiApiUrl;
+
+    @Value("${gemini.api.key}")
+    private String geminiApiKey;
+
+
+    public String getRecommendations(String details){
+        Map<String,Object> requestBody=Map.of(
+                "contents",new Object[]{
+                        Map.of("parts",new Object[]{
+                                        Map.of("text",details)
+                                })});
+
+        String response=webClient.post()
+                .uri(geminiApiUrl)
+                .header("Content-Type","application/json")
+                .header("x-goog-api-key",geminiApiKey)
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        return response;
+    }
+}
